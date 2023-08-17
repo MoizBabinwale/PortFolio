c4fde9ac05dd49e20294753b41e403a3875f06b2
MoizBabinwale <mbabinwale@gmail.com>
2023-08-17 21:02:56 +0530

    Empty the git repo

--- src/App.js
+++ src/App.js
@@ -7,26 +7,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false)
-  const [isadmin, setIsAdmin] = useState(false)
-  const loginStatus = (value) => {
-    setIsLogin(value)
-    console.log("isadmin ", isLogin);
-  }
-  const adminStatus = (value) => {
-    setIsAdmin(value)
-    console.log("isadmin ", isadmin);
-  }
   return (
     <div className="App">
-      <Router>
-        <Navbar loginStatus={loginStatus} isadmin={isadmin} isLogin={isLogin} />
-        <div>
-          {/* Your other components and routing */}
-          <ToastContainer position="top-right" theme="light" />
-          <AllRoutes loginStatus={loginStatus} adminStatus={adminStatus} isLogin={isLogin} />
-        </div>
-      </Router>
+      Hello World!
     </div>
   );
 }
