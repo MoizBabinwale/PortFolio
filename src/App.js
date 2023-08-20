25aa38842af92a9739c6ba11c7a4a033a91610ec
MoizBabinwale <mbabinwale@gmail.com>
2023-08-20 12:45:05 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
-import { BrowserRouter as Router } from "react-router-dom";
+
 import './App.css';
-import Navbar from "./components/Navbar";
-import AllRoutes from "./AllRoutes";
-import { useState } from "react";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
